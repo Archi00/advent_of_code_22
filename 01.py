b5847c2309277b2ff7e17001aecb3c694f547057
Archi00 <albert.archi@gmail.com>
2022-12-02 01:49:49 +0100

    resolved

--- 01.py
+++ 01.py
@@ -8,7 +8,8 @@ test_string = f.read()
 test = test_string.split("\n")
 test.pop()
 test.pop(0)
-tmp = []
+
+elves = []
 count_total = 0
 count = 0
 top_count = 0
@@ -16,10 +17,15 @@ for i in test:
     if len(i) > 0: 
         count_total += int(i)
     else:
-        tmp.append({"id": count, "calories": count_total})
+        elves.append({"id": count, "calories": count_total})
         if count_total > top_count:
             top_count = count_total
         count += 1
         count_total = 0
 
-print(top_count)
+top3 = sorted(elves, key=lambda d: d['calories'], reverse=True)[:3] 
+total_amount = 0
+for elf in top3:
+    total_amount += elf["calories"]
+print(top3)
+print(total_amount)
